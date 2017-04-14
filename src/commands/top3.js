2e92781b21f30edbd41eb7be271c81b2b01672c0
Vivian Low <sunset1215@users.noreply.github.com>
2017-04-14 12:11:31 +0800

    command for the first one

---
+++ src/commands/top3.js
@@ -0,0 +1,66 @@
+var bot = require('../bot');
+// need to edit the command and make it accept a parameter
+var commandRegex = /^\/top3/i;
+
+var url = "https://api.github.com/repos/tungnk1993/scrapy/stats/contributors";
+
+bot.onText(commandRegex, function(msg, match) {
+  var replyChatId = msg.chat.id;
+  if (msg.chat.type !== 'private') {
+    // this is a group message, so let's ignore it
+    return;
+  }
+  var messageOptions = { parse_mode: 'Markdown' };
+  var output = getAnswer(replyChatId, messageOptions);
+  bot.sendMessage(replyChatId,
+                  output,
+                  messageOptions);
+});
+
+function getAnswer(replyChatId, messageOptions) {
+
+  var request = require('request');
+
+  var options = {
+    url: url,
+    json: true,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = body;
+      data.sort(sortDes());
+      var index = 1;
+      var output = "Top 3 Contributors:";
+      for (var i in data) {
+        output += "\n" + index + ". " + data[i]['author']['login'];
+        index++;
+        if (i == 2) {
+          break;
+        }
+      }
+      bot.sendMessage(replyChatId,
+                  output,
+                  messageOptions);
+    } else {
+      console.log("error");
+    }
+  });
+  
+  return "Processing...";
+
+}
+
+function sortDes() {
+  return function(a,b){
+      if( a["total"] > b["total"]){
+          return -1;
+      }else if( a["total"] < b["total"] ){
+          return 1;
+      }
+      return 0;
+   }
+}
